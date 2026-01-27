e7d6e9f535b02b1630da2b27000b08ef80aec805
zolotty2 <operad.go@gmail.com>
2026-01-27 10:36:10 +0500

    html страничка !
    Signed-off-by: zolotty2 <operad.go@gmail.com>

---
+++ resources/views/index.blade.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Goods</title>
+    <link rel="shortcut icon" href="assets/img/favicon.png" type="image/x-icon">
+    <script defer src="assets/bootstrap/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+<header>
+    <nav class="mt-3 d-flex justify-content-between fs-2">
+        <a href="category.html" class="text-primary text-decoration-none">Category</a>
+        <a href="index.html" class="text-primary text-decoration-none">Goods</a>
+        <a href="orders.html" class="text-primary text-decoration-none">Orders</a>
+        <div>
+            <a href="login.html" class="login text-decoration-none">Login</a>
+        </div>
+    </nav>
+    <h1 class="d-flex justify-content-center pt-3">Goods</h1>
+</header>
+<main>
+    <section class="container">
+        <article class="card my-3">
+            <div class="card-title bg-secondary bg-gradient p-3 text-light d-flex justify-content-between">
+                <h3>Good name</h3>
+                <a href="" title="Delete good" class="btn-danger py-1 px-2 justify-content-center text-decoration-none fs-4">&#x2620;</a>
+            </div>
+            <div class="card-body d-flex justify-content-between">
+                <div class="card-img">
+                    <img src="assets/img/default.jpg" alt="Название товара">
+                </div>
+                <div class="card-info">
+                    <p class="card-text text-black-50"><span class="text-dark">About: </span>Lorem ipsum dolor sit amet,
+                        consecter.</p>
+                    <p class="card-text text-danger d-flex justify-content-end">2 499.99 ₽</p>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-end">
+                <a href="good.html" class="btn btn-primary fs-3">More about</a>
+            </div>
+        </article>
+        <article class="card my-3">
+            <div class="card-title bg-secondary bg-gradient p-3 text-light d-flex justify-content-between">
+                <h3>Good name</h3>
+                <a href="" title="Delete good" class="btn-danger py-1 px-2 justify-content-center text-decoration-none fs-4">&#x2620;</a>
+            </div>
+            <div class="card-body d-flex justify-content-between">
+                <div class="card-img">
+                    <img src="assets/img/default.jpg" alt="Название товара">
+                </div>
+                <div class="card-info">
+                    <p class="card-text text-black-50"><span class="text-dark">About: </span>Lorem ipsum dolor sit amet,
+                        consecter.</p>
+                    <p class="card-text text-danger d-flex justify-content-end">3 599.99 ₽</p>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-end">
+                <a href="good.html" class="btn btn-primary fs-3">More about</a>
+            </div>
+        </article>
+        <article class="card my-3">
+            <div class="card-title bg-secondary bg-gradient p-3 text-light d-flex justify-content-between">
+                <h3>Good name</h3>
+                <a href="" title="Delete good" class="btn-danger py-1 px-2 justify-content-center text-decoration-none fs-4">&#x2620;</a>
+            </div>
+            <div class="card-body d-flex justify-content-between">
+                <div class="card-img">
+                    <img src="assets/img/default.jpg" alt="Название товара">
+                </div>
+                <div class="card-info">
+                    <p class="card-text text-black-50"><span class="text-dark">About: </span>Lorem ipsum dolor sit amet,
+                        consecter.</p>
+                    <p class="card-text text-danger d-flex justify-content-end">6 499.99 ₽</p>
+                </div>
+            </div>
+            <div class="card-footer d-flex justify-content-end">
+                <a href="good.html" class="btn btn-primary fs-3">More about</a>
+            </div>
+        </article>
+    </section>
+</main>
+</body>
+</html>
