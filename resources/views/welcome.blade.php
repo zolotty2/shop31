8f18bb609e8b99e982238cd7d356eff08bf25c68
zolotty2 <operagx.go@gmail.com>
2026-01-27 10:42:10 +0500

    html страничка !
    Signed-off-by: zolotty2 <operad.go@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -76,6 +76,7 @@
                                         <path
                                             d="M7.70833 6.95834V2.79167H3.54167M2.5 8L7.5 3.00001"
                                             stroke="currentColor"
+
                                             stroke-linecap="square"
                                         />
                                     </svg>
