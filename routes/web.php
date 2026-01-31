2edb1c73c212cf86824d8a5b7ee4f2d7d2d80de6
zolotty2 <operad.go@gmail.com>
2026-01-31 11:53:24 +0500

    форма входа логин
    Signed-off-by: zolotty2 <operad.go@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,3 +9,7 @@ Route::get('/', function () {
 Route::get('/categories/{category}', function () {
     return view('index');
 });
+Route::get('/login',[AuthController::class,'login'])
+    ->name('login');
+Route::post('/loginSend',[AuthController::class,'loginSend'])
+    ->name('login.send');
